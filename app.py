e079f5bfdb9bb5bce00b1d85763e2dfc153534bf
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2025-08-06 02:11:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,71 +1,55 @@
 import streamlit as st
-
 import pickle
-
-import string 
-
-from nltk.corpus import stopwords
-
+import string
 import nltk
-
+import os
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-ps=PorterStemmer()
+# Use local nltk_data folder
+local_nltk_path = os.path.join(os.path.dirname(__file__), "nltk_data")
+nltk.data.path.append(local_nltk_path)
+
+print(nltk.data.path)
+
+ps = PorterStemmer()
 
 def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
 
-    text=text.lower()
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
 
-    text=nltk.word_tokenize(text)
+    text = y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words("english") and i not in string.punctuation:
+            y.append(i)
 
-    y=[]
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
 
-    for i in text:
+    return " ".join(y)
 
-        if i.isalnum():
+# Load models
+tfidf = pickle.load(open("vectorizer.pkl", "rb"))
+model = pickle.load(open("model.pkl", "rb"))
 
-          y.append(i)
+st.title("SMS Spam Classifier")
 
-    text=y[:]
-
-    y.clear()
-
-    for i in text:
-
-        if i not in stopwords.words("english") and i not in string.punctuation:
-
-          y.append(i)
-
-    text=y[:]
-
-    y.clear()
-
-    for i in text:
-
-        y.append(ps.stem(i))
-
-    return " ".join(y)
-
-tfidf=pickle.load(open("vectorizer.pkl","rb"))
-
-model=pickle.load(open("model.pkl","rb"))
-
-st.title("SMS spam detector")
-
-input=st.text_area("Enter message")
+input_sms = st.text_area("Enter the message")
 
 if st.button("Predict"):
+    transformed_sms = transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
 
-    input=transform_text(input)
-
-    input=tfidf.transform([input])
-
-    result=model.predict(input)[0]
-
-    if result==1:
-
-        st.header("Spam")
-
-    else:
-
-        st.header("Not Spam")
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
